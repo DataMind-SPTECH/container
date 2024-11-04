0a96f7628ee87b2829cf4ac214f1d9ca1d707eda
AnilmarChoque <anilmarorellana@gmail.com>
2024-11-04 01:58:25 -0300

    atualizando caminho do java

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Executa o arquivo .jar
-java -jar /app/Projeto-JAVA.jar
+# Executa o arquivo .jar usando o caminho completo para o java
+/usr/local/openjdk-21/bin/java -jar /app/Projeto-JAVA.jar
